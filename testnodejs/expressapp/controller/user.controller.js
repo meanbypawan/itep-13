311ea847e3bacf4ba4664255faffc69a10298981
mean <meanstack4@gmail.com>
2025-05-26 15:09:25 +0530

    express update 3

--- testnodejs/expressapp/controller/user.controller.js
+++ testnodejs/expressapp/controller/user.controller.js
@@ -1,17 +1,38 @@
-import pool from "../db/dbConfig.js";
+import User from "../model/user.model.js";
+
+export const signIn = (request,response,next)=>{
+  let {email,password} = request.body;
+  User.find(email,password)
+  .then(result=>{
+    if(result.length)
+      return response.redirect("/");
+    else
+      return response.redirect("/sign-in");
+  })
+  .catch(err=>{
+    return response.end("Something went wrong....");
+  })
+}
+export const checkForEmail = (request,response,next)=>{
+  User.hasEmail(request.params.emailId)
+  .then(result=>{
+    if(result.length)
+     return response.status(200).json({status: true,message: "Email id is alread taken"});
+    else
+     return response.status(200).json({status: false,message: "Available"});
+  }).catch(err=>{
+    console.log(err);
+  })
+}
 export const signUp = (request,response,next)=>{
   let {name,email,password,contact} = request.body;
-  pool.getConnection((err,con)=>{
-    if(!err){
-       let sql  = "insert into user(name,email,password,contact) values(?,?,?,?)";
-       con.query(sql,[name,email,password,contact],(err,result)=>{
-          con.release();
-          err ? console.log(err) : console.log(result);
-       });
-       
-    }
-    else{
-     console.log(err);
-    }
-  })
+  let user = new User(null,name,email,password,contact);
+  user.create().then(result=>{
+    //return response.render("signin.ejs");
+    return response.redirect("/sign-in");
+    // http://localhost:3000/sign-in
+  }).catch(err=>{
+    console.log(err);
+    response.end("Something wrong....");
+  });
 }
--- testnodejs/expressapp/routes/user.route.js
+++ testnodejs/expressapp/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signUp } from "../controller/user.controller.js";
+import { signUp,checkForEmail, signIn } from "../controller/user.controller.js";
 
 const router = express.Router();
 
 // http://localhost:3000/user/sign-up
 router.post("/signup",signUp);
+router.get("/check-for-email-id/:emailId",checkForEmail);
+router.post("/signin",signIn);
 export default router;
